2374feee07d4581b97dc721cbf6bab21fe4eebe1
Loicwr <nephtyse19@hotmail.fr>
2025-08-06 14:16:37 +0200

    Refactor WeakSpot script: Remove unused Start and Update methods, streamline OnTriggerEnter2D logic

--- Assets/Scripts/WeakSpot.cs
+++ Assets/Scripts/WeakSpot.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 
 public class WeakSpot : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject objectToDestroy;
+    // elle sert a lire quand un objet rentre dans le safespot
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(objectToDestroy);
+        }
     }
 }

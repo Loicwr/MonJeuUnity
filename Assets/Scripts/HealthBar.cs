ba16a343fc2b9bc4be5ba9c8a3eea16df582dbce
Loicwr <nephtyse19@hotmail.fr>
2025-08-06 16:09:40 +0200

    Add PlayerHealth script to manage player health and integrate with HealthBar

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,16 +5,23 @@ public class HealthBar : MonoBehaviour
 {
     public Slider slider;
 
+    public Gradient gradient;
+    public Image fill;
+
     // sert a initialiser la barre de vie
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+
+        fill.color = gradient.Evaluate(1f);
     }
 
     // Sert a indiquer le nombre de point de vie a afficher ( dégat ou soins )
     public void SetHealth(int health)
     {
         slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }

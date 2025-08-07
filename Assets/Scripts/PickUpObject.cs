bcf001942871bcccd7b82d897517733a47410812
Loicwr <nephtyse19@hotmail.fr>
2025-08-07 11:26:12 +0200

    Add Inventory class and implement object pickup functionality

--- Assets/Scripts/PickUpObject.cs
+++ Assets/Scripts/PickUpObject.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class PickUpObject : MonoBehaviour
 {
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }    
+    }
 }

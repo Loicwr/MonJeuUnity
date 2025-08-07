5a3671ac7d289d9c31d6c96c09e658b722c0383a
Loicwr <nephtyse19@hotmail.fr>
2025-08-07 12:20:04 +0200

    Add Level02 scene and implement inventory system
    - Created a new scene: Level02.
    - Enhanced Inventory class to manage coin count and display.
    - Added LoadSpecificScene script as a placeholder for future scene loading functionality.
    - Updated PickUpObject script to increment coin count upon player collision.
    - Added pixelated font asset for UI elements.
    - Updated EditorBuildSettings to include Level01 scene instead of SampleScene.

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,6 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    public 
+    public int coinsCount;
+    public Text coinsCountText;
+
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de Inventory dans la scène");
+            return;
+        }
+
+        instance = this;
+    }
+
+    public void AddCoins(int Count)
+    {
+        coinsCount += Count;
+        coinsCountText.text = coinsCount.ToString();
+    }
 }
---
+++ Assets/Scripts/LoadSpecificScene.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class LoadSpecificScene : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider other)
+    {
+        
+    }
+}

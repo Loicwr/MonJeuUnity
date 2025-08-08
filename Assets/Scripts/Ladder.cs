8b8f279f30dccbb235b5f62b065d412db11364ff
Loicwr <nephtyse19@hotmail.fr>
2025-08-08 14:00:26 +0200

    feat: Implement ladder climbing functionality and player movement adjustments

--- Assets/Scripts/Ladder.cs
+++ Assets/Scripts/Ladder.cs
@@ -2,5 +2,45 @@ using UnityEngine;
 
 public class Ladder : MonoBehaviour
 {
-    
+
+    public bool isInRange;
+    private PlayerMovement playerMovement;
+    public BoxCollider2D collider;
+
+
+    public void Awake()
+    {
+        playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+    }
+
+    public void Update()
+    {
+        if (isInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            playerMovement.isClimbing = true;
+            collider.isTrigger = true;      
+        }
+    }
+
+    // ontrigger = lire du code quand l'objet entre
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+
+    }
+
+    // ontrigger = lire du code quand l'objet sort
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = false;
+            playerMovement.isClimbing = false; 
+            collider.isTrigger = false;   
+        }
+    }
 }
+ 
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
 
     private bool isJumping;
     private bool isGrounded;
+    public bool isClimbing;
 
 
     public Transform groundCheck;
@@ -22,9 +23,16 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector3 velocity = Vector3.zero;
     private float horizontalMovement;
+    private float verticalMovement;
 
     void Update()
     {
+        // Calculer vitesse de mouvement horizontal
+        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime;
+
+        // Calculer vitesse de mouvement vertical
+        verticalMovement = Input.GetAxis("Vertical") * moveSpeed * Time.fixedDeltaTime;
+
         // Est ce que ya une demande de saut
         if (Input.GetButtonDown("Jump") && isGrounded == true)
         {
@@ -44,23 +52,31 @@ public class PlayerMovement : MonoBehaviour
         // il crée une boite de collision
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayers);
 
-        // Calculer vitesse de mouvement horizontal
-        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         // Effectuer le mouvement
-        MovePlayer(horizontalMovement);
-          }
-
-    void MovePlayer(float _horizontalMovement)
-    {
-        // Calculer vélocité de notre cible ( personnage vers le prochain mouvement)
-        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.linearVelocity.y);
-        rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity, targetVelocity, ref velocity, 0.05f);
-
-        if (isJumping == true)
-        {
-            rb.AddForce(new Vector2(0f, jumpForce));
-            isJumping = false;
+        MovePlayer(horizontalMovement, verticalMovement);
         }
+
+    void MovePlayer(float _horizontalMovement, float _verticalMovement)
+    {
+        if (!isClimbing)
+        {
+            // Calculer vélocité de notre cible ( personnage vers le prochain mouvement)
+            Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.linearVelocity.y);
+            rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity, targetVelocity, ref velocity, 0.05f);
+
+            if (isJumping)
+            {
+                rb.AddForce(new Vector2(0f, jumpForce));
+                isJumping = false;
+            }
+        }
+        else
+        {
+            //déplacement vertical
+            Vector3 targetVelocity = new Vector2(0 , _verticalMovement);
+            rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity, targetVelocity, ref velocity, 0.05f);
+        }
+
     }
 
     void Flip(float _velocity)
